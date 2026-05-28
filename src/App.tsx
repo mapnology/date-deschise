26dc188561e29ca7fb1079aae3b18c4825706ed5
mdomnita <mdomnita@gmail.com>
2026-05-28 12:26:34 +0300

    Update page titles for each page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import { Navbar } from './components/Navbar'
 import { Footer } from './components/Footer'
 import { HomePage } from './pages/HomePage'
@@ -6,9 +7,26 @@ import { CAENPage } from './pages/CAENPage'
 import { DocsPage } from './pages/DocsPage'
 import { AboutPage } from './pages/AboutPage'
 
+const PAGE_TITLES: Record<string, string> = {
+  '/': 'Date Deschise',
+  '/caen': 'Coduri CAEN Rev. 3',
+  '/documentatie': 'Documentație API',
+  '/despre': 'Despre proiect',
+}
+
+function PageTitleUpdater() {
+  const location = useLocation()
+  useEffect(() => {
+    const title = PAGE_TITLES[location.pathname] || PAGE_TITLES['/']
+    document.title = title
+  }, [location.pathname])
+  return null
+}
+
 export default function App() {
   return (
     <BrowserRouter>
+      <PageTitleUpdater />
       <div className="flex min-h-screen flex-col bg-gray-50">
         <Navbar />
         <div className="flex-1">
